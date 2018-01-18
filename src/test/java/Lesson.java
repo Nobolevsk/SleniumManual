fbde13435cf204393c1076e6904c06e414daea15
Novruz <nobolevsk@gmail.com>
2018-01-19 00:19:12 +0300

    aaa

---
+++ src/test/java/Lesson.java
@@ -0,0 +1,11 @@
+package test.java;
+
+/**
+ * Created by Новруз on 19.01.2018.
+ */
+
+public class Lesson {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
